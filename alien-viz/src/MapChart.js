9687eaa8697f0c9b7637f377b9950912b308c9b0
zhuoran <pkuyzr@gmail.com>
2018-10-27 23:28:12 -0700

    create app

---
+++ alien-viz/src/MapChart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import * as d3 from "d3";
+
+class MapChart extends Component {
+    render() {
+        return (
+            <div className="temp">
+            </div>
+        )
+    }
+}
+
+export default MapChart;
